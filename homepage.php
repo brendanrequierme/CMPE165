90694a471038330f53fbcf06efc8af95a209e5ac
Brandon Truong <73107239+BrandonTruonggg@users.noreply.github.com>
2022-03-13 00:24:22 -0800

    Add files via upload

--- homepage.php
+++ homepage.php
@@ -1,6 +1,7 @@
 <?php
 
 ?>
+
 <!DOCTYPE html>
 
 <head>
@@ -12,27 +13,120 @@
 
     <h1>Welcome to the Spartan DeathCoder Hotel</h1>
     <!-- This table is for the top part of the header -->
-    <table class = "top">
+    <table style margin = "400px" class = "top">
         <tr>
-            <th><a class = "logo" href = "http://127.0.0.1:3000/">Spartan Death Coder Hotel</a></th>
-            <td>USD</td>
-            <td>Help</td>
-            <td>Trips</td>
-            <td>Sign In</td>
+            <th><a class = "logo" href = "http://127.0.0.1:3000/">Spartan DeathCoder Hotel</a></th>
+            <td class = redTextRPad>USD</td>
+            <td class = redTextRPad>Help</td>
+            <td class = redTextRPad>Trips</td>
+            <td class = redTextRPad>Sign In</td>
         </tr> 
     </table>
+
     <!-- This table is for the bottom part of the header -->
-    <table class = "bottom">
+    <table  align = "center">
         <tr>
-            <td><a href = "https://www.w3schools.com/">Deals</a></td>
-            <td><a href = "https://www.w3schools.com/">Vacation Rentals</a></td>
-            <td><a href = "https://www.w3schools.com/">Car Rental</a></td>
-            <td><a href = "https://www.w3schools.com/">Groups & Meetings</a></td>
-            <td><a href = "https://www.w3schools.com/">Gift Cards</a></td>
-            <td><a href = "https://www.w3schools.com/">List Your Property</a></td>
-            <td><a href = "https://www.w3schools.com/">Rewards</a></td>
-            <td><a href = "https://www.w3schools.com/">Credit Card</a></td>
+            <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Deals</a></td>
+            <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Vacation Rentals</a></td>
+            <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Car Rental</a></td>
+            <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Groups & Meetings</a></td>
+            <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Gift Cards</a></td>
+            <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">List Your Property</a></td>
+            <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Rewards</a></td>
+            <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Credit Card</a></td>
         </tr>
+    </table>
+
+    <img src = "images/lakePicture.jpg" width = 100% height = "500"/>
+    <!-- This is for body/searchbar -->
+
+    <table align = "center">
+        <tr>
+            <h3>Why book with Spartan DeathCoder Hotel?</h3>
+        </tr>
+        <tr>
+            <td class = "rPad">Free Cancelation</td>
+            <td class = "rPad">Our price guarantee</td>
+            <td class = "rPad">Get a reward night</td>
+        </tr>
+    </table>
+
+    <table align = "center">
+        <tr>
+            <h3>A snapshot of the world's most picture-perfect properties</h3>
+            <td>Check out some of our favorite unique stays</td>
+        </tr>
+    </table>
+
+    <!-- Links for the hotels 
+    https://www.hotelgalvez.com/
+    https://www.baglionihotels.com/branches/baglioni-hotel-london/
+    https://www.agoda.com/l-hotel/hotel/khon-kaen-th.html?cid=1844104
+    https://www.omnihotels.com/hotels/atlanta-cnn-center
+    -->
+
+    <table align = "center">
+        <tr>
+            <td><img src = "images/hotel1.jpg" width = "300" height = "300"/></td>
+            <td><img src = "images/hotel2.jpg" width = "300" height = "300"/></td>
+            <td><img src = "images/hotel3.jpg" width = "300" height = "300"/></td>
+            <td><img src = "images/hotel4.jpeg" width = "300" height = "300"/></td>
+        </tr>
+            <td><h4 class = "noMargin">Grand Calvez</h4></td>
+            <td><h4 class = "noMargin">Baglioni Hotel</h4></td>
+            <td><h4 class = "noMargin">L Hotel</h4></td>
+            <td><h4 class = "noMargin">Omni Hotels & Resorts</h4></td>
+        </tr>
+        <tr>
+            <td>Galveston, Texas</td>
+            <td>London, UK</td>
+            <td>Khon Kaen, Thailand</td>
+            <td>Atlanta, Georgia</td>
+        </tr>
+    </table>
+
+    <table align = "center">
+        <h3>Travel with Confidence</h3>
+        <tr>
+            <td>Many properties have updated us about their enhanced health and safety measures. So, during your search, you may find details like:</td>
+        </tr>
+    </table>
+
+    <table align = "center">
+        <tr>
+            <td><h4 class = "noBotMargin">Official health standards</h4></td>
+            <td><h4 class = "noBotMargin">Social distancing</h4></td>
+        </tr>
+        <tr>
+            <td class = "rPad2">Properties adhering to corporate/organization <br>
+            sanitization guidelines.</td>
+            <td>Contactless check-in and check-out along with other social 
+            <br>distancing measure.</td>
+        </tr>
+        <tr>
+            <td><h4 class = "noBotMargin">Hygiene and sanitization</h4></td>
+            <td><h4 class = "noBotMargin">Essentials at the property</h4></td>
+        </tr>
+        <tr>
+            <td class = "rPad2">The use of disinfectant and whether properties <br>
+            enforce a gap period between stays.</td>
+            <td>Free hand sanitizer for guests and individually wrapped food.</td>
+        </tr>
+    </table>
+
+    <table align = "center">
+        <tr>
+            <h3 class = "noBotMargin">Overheard from our loyalty members</h3>
+        </tr>
+        <tr>
+            <td>We have over 50 million happy members and have given away over 25 million reward nights around the world. Here's what our members have to say</td>
+        </tr>
+    </table>
+
+    <table align = "center" class = bottom>
+        <td>About Us</td>
+        <td>List Your Property</td>
+        <td>Conact Us</td>
     </table>
 
 </body>
