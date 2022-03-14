6e06a8fa8e5284390a0c98920f5a91a2636e3860
Brandon Truong <73107239+BrandonTruonggg@users.noreply.github.com>
2022-03-13 20:18:00 -0700

    Add files via upload

--- homepage.php
+++ homepage.php
@@ -19,7 +19,7 @@
             <td class = redTextRPad>USD</td>
             <td class = redTextRPad>Help</td>
             <td class = redTextRPad>Trips</td>
-            <td><a class = "clearDecorAndRed" href = "http://localhost/CMPE165/login.php">Sign In</a></td>
+            <td><a class = redTextRPad2 href = "http://localhost/CMPE165/login.php">Sign In</a></td>
             <td class = redTextRPad>Sign Out</td>
         </tr> 
     </table>
