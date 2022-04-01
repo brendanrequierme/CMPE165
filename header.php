30ffa6c9e4849801dc7fdc7f6b8ca7388100c489
Brandon Truong <73107239+BrandonTruonggg@users.noreply.github.com>
2022-03-31 19:10:05 -0700

    Add files via upload

--- header.php
+++ header.php
@@ -16,8 +16,7 @@
             <table>
                 <tr>
                     <th class = "logo"><a class = "clearDecorAndRed" href = "homepage.php">Spartan Death Hotel</a></th>
-                    <td class = redTextRPad><a class = "clearDecorAndRed" href = "payment.php">USD</td>
-                    <td class = redTextRPad>Help</td>
+                    <td class = redTextRPad><a class = "clearDecorAndRed" href = "helppage.php">Help</a></td>
                     <td class = redTextRPad>Trips</td>
                     <td class = redTextRPad2><a class = "clearDecorAndRed" href = "login.php">Sign In</a></td>
                     <td class = redTextRPad2><a class = clearDecorAndRed href = "logout.php">Sign Out</a></td>
@@ -32,9 +31,6 @@
                 <tr>
                     <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Deals</a></td>
                     <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Vacation Rentals</a></td>
-                    <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Car Rental</a></td>
-                    <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Groups & Meetings</a></td>
-                    <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Gift Cards</a></td>
                     <td><a class = "clearDecorAndRPad" href = "listpropertiespage.php">List Your Property</a></td>
                     <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Rewards</a></td>
                     <td><a class = "clearDecorAndRPad" href = "https://www.w3schools.com/">Credit Card</a></td>
