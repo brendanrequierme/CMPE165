28d37559717003c57268ae2149f59ae4f42e4649
Brandon Truong <73107239+BrandonTruonggg@users.noreply.github.com>
2022-04-01 22:34:19 -0700

    Add files via upload

--- header.php
+++ header.php
@@ -7,6 +7,10 @@
     <head>
         <script src='http://use.edgefonts.net/nosifer.js'></script>
         <link rel="stylesheet" type="text/css" href="./styles.css" />
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
     </head>
 
     <body>
