eb5afda7c6d13489e02d6d3942304574e3f00777
Brandon Truong <73107239+BrandonTruonggg@users.noreply.github.com>
2022-04-03 20:05:46 -0700

    Add files via upload

--- helppage.php
+++ helppage.php
@@ -27,7 +27,12 @@
 
     <body>
         <div>
-            <p>There is no information to give for helping currently.</p>
+            <h2>How do you use the search bar?</h2>
+            <p>Type the word that nearest matches the hotel name. For example, If you are looking for a place like the BeachView Inn,</p>
+            <p>you can type Beach and then you would get all of the hotels that have Beach listed in the name</p>
+            <h2>How do you use the filter list?</h2>
+            <p>Go to the Booking Page and then click on the drop down menu with -------. You will see two options appear after where</p>
+            <p>you will be able to sort from low-high or high-low.
         </div>
         
         <!-- This div is for the bottom part of the page -->
